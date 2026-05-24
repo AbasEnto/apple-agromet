e1ce6e6e15ddcff5e0fc04498a2e7d559024a260
Dr. Mohd Abas Shah <khubaib20@gmail.com>
2026-05-24 21:39:44 +0530

    Create districts.js

---
+++ districts.js
@@ -0,0 +1,79 @@
+// Kashmir Apple Agro-Meteorological Dashboard
+// Representative district monitoring locations
+
+const districts = [
+
+  {
+    id: "srinagar",
+    name: "Srinagar",
+    latitude: 34.108000,
+    longitude: 74.877833
+  },
+
+  {
+    id: "shopian",
+    name: "Shopian",
+    latitude: 33.688333,
+    longitude: 74.861111
+  },
+
+  {
+    id: "pulwama",
+    name: "Pulwama",
+    latitude: 33.857306,
+    longitude: 74.851861
+  },
+
+  {
+    id: "budgam",
+    name: "Budgam",
+    latitude: 33.942083,
+    longitude: 74.802861
+  },
+
+  {
+    id: "anantnag",
+    name: "Anantnag",
+    latitude: 33.711611,
+    longitude: 75.166444
+  },
+
+  {
+    id: "kulgam",
+    name: "Kulgam",
+    latitude: 33.648528,
+    longitude: 75.025167
+  },
+
+  {
+    id: "baramulla",
+    name: "Baramulla",
+    latitude: 34.165056,
+    longitude: 74.559333
+  },
+
+  {
+    id: "kupwara",
+    name: "Kupwara",
+    latitude: 34.213639,
+    longitude: 74.506194
+  },
+
+  {
+    id: "bandipora",
+    name: "Bandipora",
+    latitude: 34.334833,
+    longitude: 74.379167
+  },
+
+  {
+    id: "ganderbal",
+    name: "Ganderbal",
+    latitude: 34.245556,
+    longitude: 74.752194
+  }
+
+];
+
+// Make available to other scripts
+window.districts = districts;
